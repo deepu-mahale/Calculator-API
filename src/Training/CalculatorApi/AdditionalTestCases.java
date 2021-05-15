c47b6d5eb2d0e7ce36cc3b405d3d560395e424d1
Deepali <deepalimahale1998@gmail.com>
2021-05-15 12:03:48 +0530

    add

---
+++ src/Training/CalculatorApi/AdditionalTestCases.java
@@ -0,0 +1,15 @@
+package Training.CalculatorApi;
+
+public class AdditionalTestCases {
+
+public void TestAdditionWithPositiveNumbers()
+{
+	int Result=Addition(10,20);
+
+}
+
+private int Addition(int i, int j) {
+	// TODO Auto-generated method stub
+	return 0;
+}
+}
